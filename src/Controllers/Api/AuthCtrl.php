a35453bbfc0006c8f3ecb0e3ad73c4d2e66bb39d
karhel <karheltmarr@hotmail.fr>
2026-01-15 11:06:50 +0100

    Authentification utilisateur et création d'un jeton JWT

--- src/Controllers/Api/AuthCtrl.php
+++ src/Controllers/Api/AuthCtrl.php
@@ -2,8 +2,12 @@
 
 namespace M2i\Blog\Controllers\Api;
 
+use M2i\Blog\Models\UserModel;
 use M2i\Blog\Traits\CanResponse;
 use M2i\Blog\Traits\Requestable;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 /**
  * Contrôleur API de gestion de la connexion utilisateur
@@ -36,7 +40,39 @@ class AuthCtrl
      */
     public function login()
     {
-        
+        $arrData = $this->getInput();
+
+        $objUserModel = new UserModel();
+        $arrUser = $objUserModel->getUserByMailAndPwd($arrData['email'], $arrData['password']);
+
+        if ($arrUser === false) 
+        {
+            // Identifiant ou mot de passe incorrect(s)
+            echo $this->jsonErrorResponse('403', "Identifiant ou mot de passe incorrect");
+        }
+        else
+        {
+            // Utilisateur trouvé
+            $key = 'votre_secret_super_long_et_securise_minimum_64_caracteres_aleatoires'; //< Clé de sécurité JWT, utilisée pour l'encodage du jeton
+
+            $payload = [
+                'user_id'       => $arrUser['user_id'],
+                'user_email'    => $arrData['email'],
+                'expires_at'    => time() + 300         //< Expire au bout de 5 minutes (5*60 secondes)
+            ];
+
+            /**
+             * IMPORTANT:
+             * You must specify supported algorithms for your application. See
+             * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+             * for a list of spec-compliant algorithms.
+             */
+            $jwt = JWT::encode($payload, $key, 'HS256');
+
+            echo $this->jsonSuccessResponse([
+                'token' => $jwt
+            ], "Connexion effectuée", 200);
+        }
     }
 
     /**
